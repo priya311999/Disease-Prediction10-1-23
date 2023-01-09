d1afbed12224c8c79da35ef144b92c92c0b4ee05
Priya Sawani <priyasawani7@gmail.com>
2023-01-10 02:16:16 +0530

    Added Admin,patient,doctor UI 10/1/23

---
+++ main_app/views.py
@@ -0,0 +1,127 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.http import JsonResponse
+from datetime import date
+
+from django.contrib import messages
+from django.contrib.auth.models import User , auth
+from .models import patient , doctor , diseaseinfo , consultation ,rating_review
+from chats.models import Chat,Feedback
+
+# Create your views here.
+
+
+#loading trained_model
+import joblib as jb
+model = jb.load('trained_model')
+
+
+
+
+def home(request):
+
+  if request.method == 'GET':
+        
+      if request.user.is_authenticated:
+        return render(request,'homepage/index.html')
+
+      else :
+        return render(request,'homepage/index.html')
+
+
+
+   
+
+       
+
+
+def admin_ui(request):
+
+    if request.method == 'GET':
+
+      if request.user.is_authenticated:
+
+        auser = request.user
+        Feedbackobj = Feedback.objects.all()
+
+        return render(request,'admin/admin_ui/admin_ui.html' , {"auser":auser,"Feedback":Feedbackobj})
+
+      else :
+        return redirect('home')
+
+
+
+    if request.method == 'POST':
+
+       return render(request,'patient/patient_ui/profile.html')
+
+
+
+
+
+def patient_ui(request):
+
+    if request.method == 'GET':
+
+      if request.user.is_authenticated:
+
+        patientusername = request.session['patientusername']
+        puser = User.objects.get(username=patientusername)
+
+        return render(request,'patient/patient_ui/profile.html' , {"puser":puser})
+
+      else :
+        return redirect('home')
+
+
+
+    if request.method == 'POST':
+
+       return render(request,'patient/patient_ui/profile.html')
+
+       
+
+
+def pviewprofile(request, patientusername):
+
+    if request.method == 'GET':
+
+          puser = User.objects.get(username=patientusername)
+
+          return render(request,'patient/view_profile/view_profile.html', {"puser":puser})
+
+
+
+def doctor_ui(request):
+
+    if request.method == 'GET':
+
+      doctorid = request.session['doctorusername']
+      duser = User.objects.get(username=doctorid)
+
+    
+      return render(request,'doctor/doctor_ui/profile.html',{"duser":duser})
+
+
+
+      
+
+
+def dviewprofile(request, doctorusername):
+
+    if request.method == 'GET':
+
+         
+         duser = User.objects.get(username=doctorusername)
+         r = rating_review.objects.filter(doctor=duser.doctor)
+       
+         return render(request,'doctor/view_profile/view_profile.html', {"duser":duser, "rate":r} )
+
+
+
+
+
+
+
+
+       
